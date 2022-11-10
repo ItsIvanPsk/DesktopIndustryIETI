c94e642125c15de6e0ca16370956e1085c4501f1
BorjaMontoro <95937006+BorjaMontoro@users.noreply.github.com>
2022-11-10 17:58:02 +0100

    Server ws (#4)
    * Add files via upload
    
    Creacion servidor con consulta a base de datos
    
    * Creación server con consulta en base de datos

---
+++ IndustryIETI/Usuario.java
@@ -0,0 +1,15 @@
+public class Usuario {
+    String username;
+    String password;
+    Usuario(String username,String password){
+        this.username=username;
+        this.password=password;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    
+}
