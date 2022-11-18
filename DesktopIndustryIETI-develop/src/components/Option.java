97ba8140c387b2983992da1a14802c2ac7fcbaed
BorjaMontoro <95937006+BorjaMontoro@users.noreply.github.com>
2022-11-18 19:32:30 +0100

    Changes model

---
+++ DesktopIndustryIETI-develop/src/components/Option.java
@@ -0,0 +1,35 @@
+package src.components;
+ 
+public class Option {
+    private int value;
+    private String text;
+    
+    //Constructor
+    public Option(int value, String text) {
+        super();
+        this.value = value;
+        this.text = text;
+    }
+    
+    //Getters and Setters
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    //ToSting
+    public String toString() {
+    	return value + "//" + text;
+    }
+}    
---
+++ DesktopIndustryIETI-develop/src/components/Sensor.java
@@ -0,0 +1,65 @@
+package src.components;
+
+public class Sensor extends Controls{
+    int id,thresholdlow,thresholdhigh;
+    private String units,text;
+
+    public Sensor(int id, String units, int thresholdlow,int thresholdhigh,String text) {
+        this.id = id;
+        this.units = units;
+        this.thresholdlow = thresholdlow;
+        this.thresholdhigh= thresholdhigh;
+        this.text = text;
+    }
+    //getters and setters
+
+    public Sensor() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+
+    public String getUnits() {
+        return units;
+    }
+
+    public void setUnits(String units) {
+        this.units = units;
+    }
+
+    public int getThresholdlow() {
+        return thresholdlow;
+    }
+
+    public void setThresholdlow(int thresholdlow) {
+        this.thresholdlow = thresholdlow;
+    }
+
+    public int getThresholdhigh() {
+        return thresholdhigh;
+    }
+
+    public void setThresholdhigh(int thresholdhigh) {
+        this.thresholdhigh = thresholdhigh;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+    @Override
+    public String toString() {
+        return "SS#"+id+"#"+units+"#"+ thresholdlow+"#"+thresholdhigh+"#"+text;
+    }
+    
+    public void setId(int id){
+        this.id = id;
+    }
+    
+    
+}
